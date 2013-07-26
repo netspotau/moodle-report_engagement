567138f7e57cd0dc78bdb40d9d95a8c0fe78b738
Adam Olley <adam.olley@netspot.com.au>
2013-07-26 13:51:05 +0930

    Don't display report link for site frontpage

--- lib.php
+++ lib.php
@@ -37,7 +37,7 @@ require_once($CFG->dirroot . '/report/engagement/locallib.php');
  * @param stdClass $context The context of the course
  */
 function report_engagement_extend_navigation_course($navigation, $course, $context) {
-    if (has_capability('report/engagement:view', $context)) {
+    if ($course->id != SITEID && has_capability('report/engagement:view', $context)) {
         $url = new moodle_url('/report/engagement/index.php', array('id' => $course->id));
         $navigation->add(get_string('pluginname', 'report_engagement'), $url,
                          navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
