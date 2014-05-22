264eb30593590d404ef26192a1ada9d002792268
Adam Olley <adam.olley@netspot.com.au>
2014-05-22 20:49:34 +0930

    Change required moodle version - supports both 2.6 and 2.7

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014042900;       // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014042400;       // Requires this Moodle version
+$plugin->version   = 2014042901;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013101800;       // Requires this Moodle version
 $plugin->component = 'report_engagement'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array('mod_engagement' => 2012031400);
 $plugin->cron      = 60;
